1a7ed9de6ed2e5c007bfb997776cb92bff2856c9
Тамара Мартышева <Tomoe.yokay730@yandex.ru>
2022-04-03 21:27:09 +0400

    database!!!!

---
+++ Hotel/HotelDatebaseImplement/Migrations/20220403171609_InitialCreate.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HotelDatebaseImplement.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Headwaiters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Headwaiters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Organizers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Organizers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lunches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dish = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Drink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeadwaiterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lunches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lunches_Headwaiters_HeadwaiterId",
+                        column: x => x.HeadwaiterId,
+                        principalTable: "Headwaiters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Level = table.Column<int>(type: "int", nullable: false),
+                    HeadwaiterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Headwaiters_HeadwaiterId",
+                        column: x => x.HeadwaiterId,
+                        principalTable: "Headwaiters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conferences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DataOf = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumberOfRooms = table.Column<int>(type: "int", nullable: false),
+                    OrganizerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conferences", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Conferences_Organizers_OrganizerId",
+                        column: x => x.OrganizerId,
+                        principalTable: "Organizers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seminars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubjectArea = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OrganizerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seminars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seminars_Organizers_OrganizerId",
+                        column: x => x.OrganizerId,
+                        principalTable: "Organizers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roomers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateBooking = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    HeadwaiterId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roomers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roomers_Headwaiters_HeadwaiterId",
+                        column: x => x.HeadwaiterId,
+                        principalTable: "Headwaiters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Roomers_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomLunches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    LunchId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomLunches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoomLunches_Lunches_LunchId",
+                        column: x => x.LunchId,
+                        principalTable: "Lunches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RoomLunches_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConferenceRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConferenceId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConferenceRooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConferenceRooms_Conferences_ConferenceId",
+                        column: x => x.ConferenceId,
+                        principalTable: "Conferences",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConferenceRooms_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConferenceSeminars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConferenceId = table.Column<int>(type: "int", nullable: false),
+                    SeminarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConferenceSeminars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ConferenceSeminars_Conferences_ConferenceId",
+                        column: x => x.ConferenceId,
+                        principalTable: "Conferences",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ConferenceSeminars_Seminars_SeminarId",
+                        column: x => x.SeminarId,
+                        principalTable: "Seminars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LunchSeminars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LunchId = table.Column<int>(type: "int", nullable: false),
+                    SeminarId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LunchSeminars", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LunchSeminars_Lunches_LunchId",
+                        column: x => x.LunchId,
+                        principalTable: "Lunches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LunchSeminars_Seminars_SeminarId",
+                        column: x => x.SeminarId,
+                        principalTable: "Seminars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Participants",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SeminarId = table.Column<int>(type: "int", nullable: false),
+                    OrganizerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Participants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Participants_Organizers_OrganizerId",
+                        column: x => x.OrganizerId,
+                        principalTable: "Organizers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Participants_Seminars_SeminarId",
+                        column: x => x.SeminarId,
+                        principalTable: "Seminars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConferenceRooms_ConferenceId",
+                table: "ConferenceRooms",
+                column: "ConferenceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConferenceRooms_RoomId",
+                table: "ConferenceRooms",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conferences_OrganizerId",
+                table: "Conferences",
+                column: "OrganizerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConferenceSeminars_ConferenceId",
+                table: "ConferenceSeminars",
+                column: "ConferenceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConferenceSeminars_SeminarId",
+                table: "ConferenceSeminars",
+                column: "SeminarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lunches_HeadwaiterId",
+                table: "Lunches",
+                column: "HeadwaiterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LunchSeminars_LunchId",
+                table: "LunchSeminars",
+                column: "LunchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LunchSeminars_SeminarId",
+                table: "LunchSeminars",
+                column: "SeminarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_OrganizerId",
+                table: "Participants",
+                column: "OrganizerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Participants_SeminarId",
+                table: "Participants",
+                column: "SeminarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roomers_HeadwaiterId",
+                table: "Roomers",
+                column: "HeadwaiterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roomers_RoomId",
+                table: "Roomers",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomLunches_LunchId",
+                table: "RoomLunches",
+                column: "LunchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomLunches_RoomId",
+                table: "RoomLunches",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rooms_HeadwaiterId",
+                table: "Rooms",
+                column: "HeadwaiterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seminars_OrganizerId",
+                table: "Seminars",
+                column: "OrganizerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConferenceRooms");
+
+            migrationBuilder.DropTable(
+                name: "ConferenceSeminars");
+
+            migrationBuilder.DropTable(
+                name: "LunchSeminars");
+
+            migrationBuilder.DropTable(
+                name: "Participants");
+
+            migrationBuilder.DropTable(
+                name: "Roomers");
+
+            migrationBuilder.DropTable(
+                name: "RoomLunches");
+
+            migrationBuilder.DropTable(
+                name: "Conferences");
+
+            migrationBuilder.DropTable(
+                name: "Seminars");
+
+            migrationBuilder.DropTable(
+                name: "Lunches");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Organizers");
+
+            migrationBuilder.DropTable(
+                name: "Headwaiters");
+        }
+    }
+}
